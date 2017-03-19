427786d8a3520f2a3512ac7c9295d56e7f98d9bd
Shinena1998 <1251632747@qq.com>
2017-03-19 23:21:24 +0800

    make it better

--- Hello.java
+++ Hello.java
@@ -1,1 +1,10 @@
-Hello.java
+public class Hello{ // 这里的对象名Hello必须与文件名相同
+    public static void main(String []args) {
+        System.out.print("Hello World!");
+    }
+}
+//使用Javac是.java文件编译成.class二进制文件。
+//再使用java对.class文件进行编译（只用写文件名，不加.class)
+//javac Hello.java
+//java Hello
+//Alt+/系统可以自动列出你想出入的命令；
